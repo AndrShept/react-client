4f38b15bc2b4597c1761b4c65312815264230a09
lolokos86 <lolokos1986@gmail.com>
2024-08-30 11:28:48 +0300

    Changes to be committed: 	modified:   index.html 	new file:   public/logo10.png 	new file:   public/logo11.png 	modified:   src/components/Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,7 @@
 import { useAuth } from '@/hooks/useAuth';
 import { MenuIcon } from 'lucide-react';
 import { Link } from 'react-router-dom';
+import { useMediaQuery } from 'usehooks-ts';
 
 import { ProfileAvatar } from './ProfileAvatar';
 import { SheetSidebar } from './SheetSidebar';
@@ -9,6 +10,7 @@ import { Button } from './ui/button';
 
 export const Navbar = () => {
   const { isAuthenticated } = useAuth();
+  const isMobile = useMediaQuery('(min-width: 768px)');
 
   return (
     <header className="h-14 w-full fixed z-50 flex justify-between top-0 bg-secondary/50 border-b  p-2 backdrop-blur-md ">
@@ -19,8 +21,17 @@ export const Navbar = () => {
           </Button>
         </SheetSidebar>
 
-        <Button className="font-semibold" asChild variant={'ghost'}>
-          <Link to={'/'}>SOCIAL MEDIA</Link>
+        <Button
+          className="font-semibold"
+          asChild
+          variant={'ghost'}
+          size={!isMobile ? 'icon' : 'default'}
+        >
+          <Link to={'/'}>
+            <img className="size-7" src="/logo11.png" alt="logo" />
+
+            <span className="md:inline-block hidden ml-2">SOCIAL MEDIA</span>
+          </Link>
         </Button>
       </div>
 
