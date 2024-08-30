a32eb26c71bc615fc87ac6be03556b915a507c9d
lolokos86 <lolokos1986@gmail.com>
2024-08-30 14:57:09 +0300

    Changes to be committed: 	modified:   .gitignore 	modified:   src/components/comment/CommentLikeIcon.tsx 	modified:   tsconfig.json

--- src/components/comment/CommentLikeIcon.tsx
+++ src/components/comment/CommentLikeIcon.tsx
@@ -1,60 +1,59 @@
-import { useLazyGetCommentsQuery } from '@/lib/services/commentApi';
-import { useLikeCommentMutation } from '@/lib/services/likeApi';
-import { useLazyGetPostByIdQuery } from '@/lib/services/postApi';
-import { useLazyGetReplysQuery } from '@/lib/services/replyApi';
-import { cn } from '@/lib/utils';
-import { ThumbsUpIcon } from 'lucide-react';
-import React from 'react';
-import { toast } from 'sonner';
+// import { useLazyGetCommentsQuery } from '@/lib/services/commentApi';
+// import { useLazyGetPostByIdQuery } from '@/lib/services/postApi';
+// import { useLazyGetReplysQuery } from '@/lib/services/replyApi';
+// import { cn } from '@/lib/utils';
+// import { ThumbsUpIcon } from 'lucide-react';
+// import React from 'react';
+// import { toast } from 'sonner';
 
-import { Button } from '../ui/button';
+// import { Button } from '../ui/button';
 
-interface CommentLikeIconProps {
-  commentId: string;
-  postId: string;
-  isCommentLikeExist: boolean;
-  likeCount: number;
-}
-export const CommentLikeIcon = ({
-  commentId,
-  postId,
-  isCommentLikeExist,
-  likeCount,
-}: CommentLikeIconProps) => {
-  const [addCommentLike, { isLoading }] = useLikeCommentMutation();
-  const [refetchComments] = useLazyGetCommentsQuery();
-  const [refetchReplys] = useLazyGetReplysQuery();
+// interface CommentLikeIconProps {
+//   commentId: string;
+//   postId: string;
+//   isCommentLikeExist: boolean;
+//   likeCount: number;
+// }
+// export const CommentLikeIcon = ({
+//   commentId,
+//   postId,
+//   isCommentLikeExist,
+//   likeCount,
+// }: CommentLikeIconProps) => {
+//   const [addCommentLike, { isLoading }] = useLikeCommentMutation();
+//   const [refetchComments] = useLazyGetCommentsQuery();
+//   const [refetchReplys] = useLazyGetReplysQuery();
 
-  const handleCommentLike = async () => {
-    try {
-      await addCommentLike(commentId).unwrap();
-      postId && (await refetchComments(postId));
-      !postId && (await refetchReplys(commentId));
-    } catch (error) {
-      toast.error('Something went wrong');
-    }
-  };
-  return (
-    <section className="flex  items-center">
-      <Button
-        disabled={isLoading}
-        onClick={handleCommentLike}
-        variant={isCommentLikeExist ? 'indigo' : 'ghost'}
-        size={'icon'}
-        className={cn(
-          'size-[26px] rounded-full focus:scale-110 transition-all',
-          {},
-        )}
-      >
-        <ThumbsUpIcon
-          className={cn('size-[18px] ', {
-            '': isCommentLikeExist,
-          })}
-        />
-      </Button>
-      {!!likeCount && (
-        <p className="text-muted-foreground text-xs ml-[2px]">{likeCount}</p>
-      )}
-    </section>
-  );
-};
+//   const handleCommentLike = async () => {
+//     try {
+//       await addCommentLike(commentId).unwrap();
+//       postId && (await refetchComments(postId));
+//       !postId && (await refetchReplys(commentId));
+//     } catch (error) {
+//       toast.error('Something went wrong');
+//     }
+//   };
+//   return (
+//     <section className="flex  items-center">
+//       <Button
+//         disabled={isLoading}
+//         onClick={handleCommentLike}
+//         variant={isCommentLikeExist ? 'indigo' : 'ghost'}
+//         size={'icon'}
+//         className={cn(
+//           'size-[26px] rounded-full focus:scale-110 transition-all',
+//           {},
+//         )}
+//       >
+//         <ThumbsUpIcon
+//           className={cn('size-[18px] ', {
+//             '': isCommentLikeExist,
+//           })}
+//         />
+//       </Button>
+//       {!!likeCount && (
+//         <p className="text-muted-foreground text-xs ml-[2px]">{likeCount}</p>
+//       )}
+//     </section>
+//   );
+// };
